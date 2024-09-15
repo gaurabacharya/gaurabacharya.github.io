84d2a87da0261c767f619e5505038080425e068b
gaurabacharya <acharyaagaurab@gmail.com>
2024-09-14 22:22:59 -0700

    Added config for github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
     output: 'export',
     distDir: 'dist',
     images: {
         unoptimized: true,
-    }
+    },
+    assetPrefix: isProd ? '/gaurabacharya.github.io/': '',
+    basePath: isProd ? '/gaurabacharya.github.io/': '',
+    trailingSlash: true,
 };
 
 export default nextConfig;

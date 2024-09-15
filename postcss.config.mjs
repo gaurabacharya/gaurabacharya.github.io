2c4ccc4728c3be3f46dd1acbf661b4faee3467fe
gaurabacharya <acharyaagaurab@gmail.com>
2024-09-14 23:26:48 -0700

    changed configs to include imports to postcss.config

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('postcss-load-config').Config} */
+import autoprefixer from 'autoprefixer';
+import tailwindcss from 'tailwindcss';
+import postcssImport from 'postcss-import';
+
 const config = {
-  plugins: {
-    tailwindcss: {},
-    'postcss-import': {}, 
-    autoprefixer: {},
-  },
+  plugins: [
+    autoprefixer(),
+    tailwindcss(),
+    postcssImport(),
+  ],
 };
 
 export default config;

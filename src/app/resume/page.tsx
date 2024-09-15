73fbafa3c6a15e1e8c4199947b00f32c4dd246ad
gaurabacharya <acharyaagaurab@gmail.com>
2024-09-14 22:48:29 -0700

    Added fix for resume page import

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../../components/header';
-import styles from './resume.module.css'
+import styles from './Resume.module.css'
 
 
 const DownloadPdf = () => {
@@ -8,11 +8,6 @@ const DownloadPdf = () => {
     <div className={styles.pageSection}>
       <Header/>
       <iframe className={styles.iframe} src="/pdf/Gaurab_Acharya_Resume.pdf"/>
-      {/* <h1>Download PDF</h1>
-      <p>Click the link below to download the PDF file:</p>
-      <a href="/pdf/Gaurab_Acharya_Resume.pdf" download className={styles.downloadLink}>
-        Download PDF
-      </a> */}
     </div>
   );
 };

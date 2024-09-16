95e99846c7ac8e2c57952fbd0a8dc9fe53b52850
gaurabacharya <acharyaagaurab@gmail.com>
2024-09-16 11:13:28 -0700

    Fixed base path and asset path and build now works

--- next.config.mjs
+++ next.config.mjs
@@ -5,9 +5,10 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
     output: 'export',
     distDir: 'dist',
-    assetPrefix: isProd ? '/gaurabacharya.github.io/': '',
-    basePath: isProd ? '/gaurabacharya.github.io': '',
+    assetPrefix: isProd ? '/': '',
+    basePath: isProd ? '': '',
     trailingSlash: true,
+    images: {unoptimized: true},
 };
 
 export default nextConfig;

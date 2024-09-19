62be21f56b16d4eefab4d9e3883acf14db314873
gaurabacharya <acharyaagaurab@gmail.com>
2024-09-18 19:55:43 -0700

    Changed about me

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,14 +7,15 @@ const AboutPage = () => {
 
   const briefSummary1 = "I'm Gaurab Acharya and I'm a senior undergraduate student " +
                        "studying biomedical engineering with a specialization in systems and signals. " +
-                       "I grew up in Canada and growing up, I played " +
-                       "competive basketball from primary school until the end of secondary school. Through learning how to train in basketball, I loved learning " +
-                       "about the human body. " +
+                       "Growing up, my main interest was playing " +
+                       "basketball from primary school and then competitively until the end of secondary school. " +
+                       "Through learning how to train in basketball, I learned a lot about optimizing atheletic performance and I was interested in learning about biology. " +
                        "I also had an interest in math and technology in school, so when exploring further education, Biomedical Engineering was the perfect option to explore my interests. ";
+
   const briefSummary2 = "Throughout university, I've taken countless courses focusing on Computer Science, Computer and Electrical Engineering, " +
-                       "Biology, Chemistry, and Biomedical Engineering to apply these skills in medical setting, providing me a variety of knowledge and experiences. " +
-                       "I've also gained industry experience, in software, at 2 companies working almost 2 years within the time of my degree, which " +
-                       "expanding my knowledge and skillset. In my free time I continue to explore my interests by coaching youth basketball in my local community " +
+                       "Biology, Chemistry, and Biomedical Engineering. I learned how to apply these skills in a medical setting providing me experience in many different fields. " +
+                       "I've also gained industry experience that grew my skillset, working as a software developer at 2 companies within the time of my degree." +
+                       "In my free time I continue to explore my interests by coaching youth basketball in my local community " +
                        "and developing personal software projects. As I am nearing graduation, I'm looking for opportunties in the industry to make an impact with " +
                        "software and continuing to explore my curiosity.";
 
